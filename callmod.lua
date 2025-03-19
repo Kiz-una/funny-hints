ae2998e301fcc3375c26a53fcf97967cc0103323
Kizuna <thatemobun@gmail.com>
2025-03-19 01:40:17 +0100

    Added 30 hints. Changed 3 hints. Removed 2 hints. Added support for each mid_text hint to have different cooldowns. Increased the default cooldown for mid_text hints to 30s. Lowered the cooldown of the civilian killed hint to 2s. Added an icon for the mod.

--- callmod.lua
+++ callmod.lua
@@ -3,7 +3,7 @@ if not Global.custom_hints then
 end
 
 Global.custom_hints.callmod = {
-    hint_call = { "$PLAYER is trying to get your attention.", "Hey! $PLAYER wants something.", "$PLAYER is yelling at you.", "Go check what $PLAYER wants.", "$PLAYER is calling you.", "$PLAYER orders you to come.", "$PLAYER wants you at their heel.", "$PLAYER wants you.", "$PLAYER wants teamplay. What a weirdo.", "$PLAYER needs your cooperation." }
+    hint_call = { "$PLAYER is trying to get your attention.", "Hey! $PLAYER wants something.", "$PLAYER is yelling at you.", "Go check what $PLAYER wants.", "$PLAYER is calling you.", "$PLAYER orders you to come.", "$PLAYER wants you at their heel.", "$PLAYER wants you.", "$PLAYER wants teamplay. What a weirdo.", "$PLAYER needs your cooperation.", "Your presence is needed by $PLAYER.", "$PLAYER needs you right now." }
 }
 Hooks:PreHook(UnitNetworkHandler, "long_dis_interaction", "FunnyHints_callmod", function ()
     SetHeistSpecificHints()
--- flash.lua
+++ flash.lua
@@ -1,11 +1,11 @@
 Global.custom_hints.flash = {
-    hint_flash = { "SEGA", "You have a little private time with me now. :)", "I can hear a horse carriage.", "That's embarrassing.", "That's going in my cringe compilation.", "Right in the optics!", "That wasn't a nice present.", "There there. I'm here for you.", "You poor thing.", "Damn gas makes me cry.", "Just wear a blindfold.", "Look away from those.", "Think faster chucklenut.", "Your opinion, my choice.", "Get 'banged! That doesn't sound right...", "Isn't flashing someone illegal?", "My eyes!", "We'll be right back." }
+    hint_flash = { "SEGA", "You have a little private time with me now. :)", "I can hear a horse carriage.", "That's embarrassing.", "That's going in my cringe compilation.", "Right in the optics!", "That wasn't a nice present.", "There there. I'm here for you.", "You poor thing.", "Damn gas makes me cry.", "Just wear a blindfold.", "Look away from those.", "Think faster chucklenut.", "Your opinion, my choice.", "Get 'banged! That doesn't sound right...", "Isn't flashing someone illegal?", "My eyes!", "We'll be right back.", "Minor setback.", "They're so tacticool.", "They got you good.", "Pocket sand!" }
 }
 Hooks:PostHook(PlayerDamage, "on_flashbanged", "FunnyHints_flash", function ()
     SetHeistSpecificHints()
 
     local flash_strength = math.pow(math.min(managers.environment_controller._current_flashbang, 1), 16) + math.min(managers.environment_controller._current_flashbang_flash, 1)
-    if flash_strength >= 2 then
+    if flash_strength >= 2 then -- Max flash strength
         local message = HintRandom("hint_flash", Global.custom_hints.flash.hint_flash)
         Global.last_hint = {
             time = Application:time(),
