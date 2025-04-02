04698a690a1dfac6c861726add63c0a42331a382
Kizuna <thatemobun@gmail.com>
2025-04-03 00:23:41 +0200

    Added 50 hints. Changed 2 hints. Changed how the welcome hint modifier works. Update version.

--- effects.lua
+++ effects.lua
@@ -59,7 +59,7 @@ end
 local heist_specific_overrides = {
 	four_stores = {
 		main = {
-			hud_hint_grabbed_small_loot = { "Progress!", "This stuff isn't worth much, but Vlad doesn't care.", "This is the greatest heist.", "This is totally worth it.", "If only I was justified to insult you for this.", "Useful for once.", "Munnie!", "Holla Holla get Dollar.", "These stores have so little money.", "Four Stores? More like Four Dollars!", "I'd rather rob a 7-Eleven.", "Cha-Ching!", "Cash is Queen.", "Greed is Good!", "Look at that wad!" }
+			hud_hint_grabbed_small_loot = { "Progress!", "This stuff isn't worth much, but Vlad doesn't care.", "This is the greatest heist.", "This is totally worth it.", "If only I was justified to insult you for this.", "Useful for once.", "Munnie!", "Holla Holla get Dollar.", "These stores have so little money.", "Four Stores? More like Four Dollars!", "I'd rather rob a 7-Eleven.", "Cha-Ching!", "Cash is Queen.", "Greed is Good!", "Look at that wad!", "It ain't much, but it's honest work." }
 		}
 	}
 }
@@ -141,11 +141,11 @@ local function on_heist_start()
         welcome_help --[[Prison Nightmare]] = {  },
         welcome_haunted --[[Safe house Nightmare]] = { "Oh, this old place.", "Did you prepare yourself for this?", "I see death." },
         welcome_run --[[Heat Street]] = { "You wouldn't need the laundromat if Hoxton didn't burn the old place down.", "What's the chance of you both meeting at the same laundromat at the same time?" },
-        welcome_hox_1 --[[The Breakout]] = {  },
+        welcome_hox_1 --[[The Breakout]] = { "I never liked Houston.", "I'm sure Hox won't be too upset about his face." },
         welcome_spa --[[Brooklyn 10-10]] = {  },
         welcome_vit --[[The White House]] = { "THE BIG ONE!" },
-        welcome_mia_2 --[[Four Floors]] = {  },
-        welcome_ranc --[[Midland Ranch]] = {  },
+        welcome_mia_2 --[[Four Floors]] = { "Imma living in a box. Imma living in a cardboard box.", "You're not Snake!" },
+        welcome_ranc --[[Midland Ranch]] = { "Yeehaw!", "Say the line Dallas!" },
         welcome_big --[[The Big Bank]] = {  },
         welcome_brb --[[Brooklyn Bank]] = {  },
         welcome_skm_run --[[Holdout: Heat Street]] = { "I miss the gasoline smell." },
@@ -174,11 +174,11 @@ local function on_heist_start()
         welcome_firestarter_3 --[[Trustee Bank]] = {  },
         welcome_peta --[[This was not the deal]] = { "This is a new low.", "I hope you're an experienced rustler." },
         welcome_deep --[[Crude Awakening]] = { "Tactical espionage action.", "Now this is Big Oil!" },
-        welcome_alex_3 --[[Bus Stop]] = {  },
+        welcome_alex_3 --[[Bus Stop]] = { "End of the line, fellas!", "Last stop!" },
         welcome_rat --[[Cook Off]] = { "How long are you planning to stay this time?", "Didn't get enough of this place?" },
         welcome_arena --[[The Alesso Heist]] = { "You can set this as your menu music.", "This guy is for real. As in he exists.", "Have you ever asked yourself why the concert above, muffles the explosions three floors down?", "They have really thin walls here." },
         welcome_red2 --[[First World Bank]] = { "This is where it all started.", "Is there a Third World Bank?", "Is this the first bank in the world?" },
-        welcome_jolly --[[Aftershock]] = { "Doing this, makes you are partly responsible for lootboxes.", "I love Vlad, but fuck him for these safes." },
+        welcome_jolly --[[Aftershock]] = { "Doing this, makes you are partly responsible for lootboxes.", "I love Vlad, but fuck him for these safes.", "You're gonna need a chiropractor after this." },
         welcome_framing_frame_3 --[[Framing]] = { "Make sure to steal his wine." },
         welcome_firestarter_2 --[[FBI Server]] = {  },
         welcome_framing_frame_1 --[[Art Gallery]] = { "The senator needs his porn in painting form.", "Good thing the senator never heard of PornHub." },
@@ -186,7 +186,7 @@ local function on_heist_start()
         welcome_alex_1 --[[Cook Off]] = { "We need to cook." },
         welcome_tag --[[Breakin' Feds]] = {  },
         welcome_hox_2 --[[The Search]] = { "Payday Gang! Open up!" },
-        welcome_mad --[[Boiling Point]] = { "Welcome to the motherland!" },
+        welcome_mad --[[Boiling Point]] = { "Welcome to the motherland!", "I hate gravity!" },
         welcome_welcome_to_the_jungle_2 --[[Engine Problem]] = { "Don't forget to search for the engine notes.", "Were about to undo years of progress." },
         welcome_skm_red2 --[[Holdout: First World Bank]] = {  },
         welcome_short1_stage1 --[[Stealth - Flash Drive]] = {  },
@@ -194,7 +194,7 @@ local function on_heist_start()
         welcome_mex_cooking --[[Coyopa Compound]] = { "This is just cook off but harder.", "Why did you come back here?" },
         welcome_shoutout_raid --[[Meltdown]] = {  },
         welcome_chew --[[Interception]] = { "Choo Choo!" },
-        welcome_kenaz --[[Golden Grin Casino]] = { "Let's go gambling!" },
+        welcome_kenaz --[[Golden Grin Casino]] = { "Let's go gambling!", "If gambling is a disease, is this a hospital?" },
         welcome_cage --[[Car Shop]] = {  },
         welcome_trai --[[Lost in Transit]] = {  },
         welcome_firestarter_1 --[[Airport]] = {  },
@@ -213,7 +213,7 @@ local function on_heist_start()
         welcome_election_day_1 --[[Right Track]] = { "Speedrun?" },
         welcome_bph --[[Hell's Island]] = { "I will be so upset if something happens to Bain.", "We're gonna save Bain and he's gonna live happily ever after." },
         welcome_branchbank --[[Bank Heist]] = { "The classic.", "This one's so boring." },
-        welcome_election_day_3 --[[Breaking Ballot]] = {  },
+        welcome_election_day_3 --[[Breaking Ballot]] = { "McKendrick better still appreciate this." },
         welcome_framing_frame_2 --[[Train Trade]] = {  },
         welcome_sah --[[Shacklethorne Auction]] = { "Where's your umbrella?" },
         welcome_chill --[[Safe House]] = { "Home sweet home." },
@@ -226,7 +226,7 @@ local function on_heist_start()
         welcome_crojob2 --[[The Bomb: Dockyard]] = { "Why didn't we just bring a ladder? We could board the Moretta already." },
         welcome_skm_arena --[[Holdout: The Alesso Heist]] = { "Did the conecert finish? I don't hear his music." },
         welcome_pbr --[[Beneath the Mountain]] = { "See? They have a mountain base. We're not the evil ones." },
-        welcome_pbr2 --[[Birth of Sky]] = { "It's kinda cramped in here.", "Are we there yet? My back hurts." },
+        welcome_pbr2 --[[Birth of Sky]] = { "It's kinda cramped in here.", "Are we there yet? My back hurts.", "There better not be any motherfucking snakes on this motherfucking plane!" },
         welcome_nail --[[Lab Rats]] = { "Rats, rats, we're the rats.", "We prey at night, we stalk at night, we're the rats." },
         welcome_peta2 --[[Dirty work]] = { "o.o", "Do we... have to be here?", "Please. Someone stop him.", "I didn't sign up for biology class!" },
         welcome_mia_1 --[[Hotline Miami]] = { "Does this count as a sequel to Mallcrasher?" },
@@ -282,36 +282,39 @@ local function on_heist_start()
         enemy_test = "testing_map",
         modders_devmap = "testing_map"
     }
-    if not Global.hint_weights.welcome_hint_modifier then
-        Global.hint_weights.welcome_hint_modifier = 1
+    if not Global.hint_weights.welcome_hint_modifiers then
+        Global.hint_weights.welcome_hint_modifiers = {}
     end
-    local modifier_step = 0.7 / 60
-    local function increase_chance_loop()
-        DelayedCalls:Add("welcome_hint_chance_increase", 60, function()
-            Global.hint_weights.welcome_hint_modifier = Global.hint_weights.welcome_hint_modifier + modifier_step
-            increase_chance_loop()
-        end)
-    end
-
     local level_id = shared_level_id[Global.game_settings.level_id] or Global.game_settings.level_id
     local is_testing_map = level_id == "testing_map"
     local hint_id = "welcome_" .. level_id
     if CustomHints.welcome[hint_id] and #CustomHints.welcome[hint_id] > 0 then
-        local chance = #CustomHints.welcome[hint_id] / 10 * Global.hint_weights.welcome_hint_modifier
+        local modifier = Global.hint_weights.welcome_hint_modifiers[hint_id] or 0
+        local chance = #CustomHints.welcome[hint_id] / 20 + modifier
         if is_testing_map then
             chance = 0.05
         end
+        LogPrivateChat("Welcome hint chance is " .. chance * 100 .. "%. Modifier: +" .. modifier * 100 .. "%.")
         if math.random() < chance then
+            LogPrivateChat("Showing welcome hint...")
             DelayedCalls:Add("welcome_hint", 5, function()
                 ShowHintCustom(hint_id, "welcome")
             end)
-            Global.hint_weights.welcome_hint_modifier = 1 - modifier_step * 15
+            Global.hint_weights.welcome_hint_modifiers[hint_id] = 0
+        else
+            local function increase_modifier()
+                Global.hint_weights.welcome_hint_modifiers[hint_id] = modifier + 0.075
+                LogPrivateChat("Inceased modifier to " .. Global.hint_weights.welcome_hint_modifiers[hint_id] * 100 .. "%.")
+            end
+            DelayedCalls:Add("welcome_hint_modifier1", 240, function()
+                increase_modifier()
+            end)
+            DelayedCalls:Add("welcome_hint_modifier2", 1200, function()
+                increase_modifier()
+            end)
         end
     else
         LogPrivateChat("This heist has no welcome hint messages.")
-    end
-    if not is_testing_map then
-        increase_chance_loop()
     end
 
     on_heist_start_finished = true
@@ -326,7 +329,7 @@ CustomHints.effects = {
 
     hint_last_life_replenished_end = { "I'm wasting my breath on you.", "You're godawful. I'm leaving.", "I've seen enough of you. Goodbye." },
 
-    hint_flash = { "SEGA", "You have a little private time with me now. :)", "I can hear a horse carriage.", "That's embarrassing.", "That's going in my cringe compilation.", "Right in the optics!", "That wasn't a nice present.", "There there. I'm here for you.", "You poor thing.", "Damn gas makes me cry.", "Just wear a blindfold.", "Look away from those.", "Think faster chucklenut.", "Your opinion, my choice.", "Get 'banged! That doesn't sound right...", "Isn't flashing someone illegal?", "My eyes!", "We'll be right back.", "A minor setback.", "They're so tacticool.", "They got you good.", "Pocket sand!", "You took the full brunt of that.", "You like eating flashes?", "It's possible to avoid getting flashed this badly, you know.", "That was avoidable.", "Did you not see that coming?", "How're you gonna know what you're aiming at like this?", ":(", "Well this sucks." }
+    hint_flash = { "SEGA", "You have a little private time with me now. :)", "I can hear a horse carriage.", "That's embarrassing.", "That's going in my cringe compilation.", "Right in the optics!", "That wasn't a nice present.", "There there. I'm here for you.", "You poor thing.", "Damn gas makes me cry.", "Just wear a blindfold.", "Look away from those.", "Think faster chucklenut.", "Your opinion, my choice.", "Get 'banged! That doesn't sound right...", "Isn't flashing someone illegal?", "My eyes!", "We'll be right back.", "A minor setback.", "They're so tacticool.", "They got you good.", "Pocket sand!", "You took the full brunt of that.", "You like eating flashes?", "It's possible to avoid getting flashed this badly, you know.", "That was avoidable.", "Did you not see that coming?", "How're you gonna know what you're aiming at like this?", ":(", "Well this sucks.", "Bang the flash before it bangs you.", "Just close your eyes, dumbass." }
 }
 InsertMessages({{ids = {"hint_last_life_replenished_1"}, messages = CustomHints.effects.hint_last_life_replenished_0}}, "effects")
 
